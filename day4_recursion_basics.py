476facc33b61a3bf95423cee689e39815e933d0b
Yashbhame_018 <yashbhame27@gmail.com>
2026-07-29 16:05:58 +0530

    Recursion more practice sums solved

--- day4_recursion_basics.py
+++ day4_recursion_basics.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     def printN(self,count,n):
         if count > n:
@@ -10,4 +11,22 @@ if __name__ == "__main__":
     n = 6
 
     sol.printN(1,n)
-    print()    
+    print()   
+
+#sum of first n natural numbers
+class Sum:
+    def SumOfN(self,n):
+        if n == 1:
+            return 1
+        return n + self.SumOfN(n - 1)
+obj2 = Sum()
+print(obj2.SumOfN(5))
+'''
+class factorial:
+    def fact(self,n):
+        if n == 1:
+            return 1
+        return n * self.fact(n - 1)
+
+obj3 = factorial()
+print(obj3.fact(5))
